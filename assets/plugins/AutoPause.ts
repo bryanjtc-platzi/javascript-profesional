2563b7c97c3dbe4a0502c7b328b21f6174328e53
Bryan Thomas <bryan.thomas25000@icloud.com>
2021-03-05 23:43:42 -0500

    avance

--- assets/plugins/AutoPause.ts
+++ assets/plugins/AutoPause.ts
@@ -1,4 +1,8 @@
+import MediaPlayer from "../MediaPlayer.js";
 class AutoPause {
+  private threshold: number;
+  player: MediaPlayer;
+
   constructor() {
     this.threshold = 0.25;
     this.handleIntersection = this.handleIntersection.bind(this);
@@ -17,7 +21,7 @@ class AutoPause {
     document.addEventListener("visibilitychange", this.handleVisibilityChange);
   }
 
-  handleIntersection(entries) {
+  private handleIntersection(entries: IntersectionObserverEntry[]) {
     const entry = entries[0];
 
     const isVisible = entry.intersectionRatio >= this.threshold;
@@ -29,7 +33,7 @@ class AutoPause {
     }
   }
 
-  handleVisibilityChange() {
+  private handleVisibilityChange() {
     const isVisible = document.visibilityState === "visible";
     if (isVisible) {
       this.player.play();
